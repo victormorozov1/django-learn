60f5a16e0d6c49d5d1aedc2d749dfbc026991d9a
victormorozov1 <vismo@bk.ru>
2022-09-11 01:08:53 +0500

    add home page and name of the link on home page

--- djangoLearn/urls.py
+++ djangoLearn/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.views import main_page
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponse('Home page'), name='home'),
     path('users', include('users.urls')),
 ]
