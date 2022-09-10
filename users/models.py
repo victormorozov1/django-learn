c0471b77ba129b7b08901cdea2746a357ecd2c64
victormorozov1 <vismo@bk.ru>
2022-09-11 00:18:09 +0500

    add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    max_password_len = 123
+
+    name = models.CharField(max_length=17)
+    password = models.CharField(max_length=max_password_len, blank=True)
+    hashed_password = models.CharField(max_length=max_password_len)
+
+    def __str__(self):
+        return f'User {self.name}'
