9d5d7a2346519a8ac6cdbd72812da1e7fcbf2a6a
victormorozov1 <vismo@bk.ru>
2022-09-12 20:46:42 +0500

    add unique name validator

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import User
 from .hash import hash
+from django.core.exceptions import ValidationError
 
 style = forms.TextInput(attrs={'class': 'form-control'})
 
@@ -10,13 +11,17 @@ class RegisterForm(forms.Form):
     password = forms.CharField(max_length=User.max_password_len, widget=style)
     repeat_password = forms.CharField(max_length=User.max_password_len, widget=style)
 
-    def clean(self):  # Function for custom validators
-        # Нужно добавить проверку на уникальность имени
+    def clean(self):
         form_data = self.cleaned_data
         if form_data['password'] != form_data['repeat_password']:
             self._errors["repeat_password"] = ["Password do not match"]
             form_data['repeat_password'] = ''  # Не работает почему-то !!!
         return form_data
+
+    def clean_name(self):
+        if len(User.objects.filter(name=self.cleaned_data['name'])) >= 1:
+            self._errors["name"] = ["This name is already occupied"]
+        return self.cleaned_data['name']
 
 
 class EnterForm(forms.Form):
