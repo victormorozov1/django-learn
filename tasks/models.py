45b2bc7c4b9a59320ecd881bbe27a45e21b7daa2
victormorozov1 <vismo@bk.ru>
2022-09-18 00:40:33 +0500

    add Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=120, default='Task')
+    description = models.TextField()
+    likes = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Task {self.title}: {self.description[:20:]}'
+
+
