0434ea6337b967413d5829ec1b5afce74c5c7788
victormorozov1 <vismo@bk.ru>
2022-09-18 21:11:46 +0500

    change Task model and add DetailAnswer model

--- tasks/models.py
+++ tasks/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Посмотреть про удаление!
+
 
 class Task(models.Model):
     title = models.CharField(max_length=120, default='Task')
     description = models.TextField()
     likes = models.IntegerField(default=0)
+    answer_type = models.CharField(max_length=20, null=True)  # detailed_answer or short_answer
+    reference_short_answer = models.TextField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Task {self.title}: {self.description[:20:]}'
+
+
+class DetailedAnswer(models.Model):
+    text = models.TextField()
+    responding_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
